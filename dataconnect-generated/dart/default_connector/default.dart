0d5d60516a572553ca672630844dbc33d3abcde7
Suvorov <sertx.fallen@bk.ru>
2024-11-01 04:14:26 +0400

    1 11

--- dataconnect-generated/dart/default_connector/default.dart
+++ dataconnect-generated/dart/default_connector/default.dart
@@ -1,30 +1,30 @@
-library default_connector;
-import 'package:firebase_data_connect/firebase_data_connect.dart';
-import 'dart:convert';
-
-
-
-
-
-
-
-class DefaultConnector {
-  
-
-  static ConnectorConfig connectorConfig = ConnectorConfig(
-    'us-central1',
-    'default',
-    'trailpro_planning',
-  );
-
-  DefaultConnector({required this.dataConnect});
-  static DefaultConnector get instance {
-    return DefaultConnector(
-        dataConnect: FirebaseDataConnect.instanceFor(
-            connectorConfig: connectorConfig,
-            sdkType: CallerSDKType.generated));
-  }
-
-  FirebaseDataConnect dataConnect;
-}
-
+// library default_connector;
+// import 'package:firebase_data_connect/firebase_data_connect.dart';
+// import 'dart:convert';
+//
+//
+//
+//
+//
+//
+//
+// class DefaultConnector {
+//
+//
+//   static ConnectorConfig connectorConfig = ConnectorConfig(
+//     'us-central1',
+//     'default',
+//     'trailpro_planning',
+//   );
+//
+//   DefaultConnector({required this.dataConnect});
+//   static DefaultConnector get instance {
+//     return DefaultConnector(
+//         dataConnect: FirebaseDataConnect.instanceFor(
+//             connectorConfig: connectorConfig,
+//             sdkType: CallerSDKType.generated));
+//   }
+//
+//   FirebaseDataConnect dataConnect;
+// }
+//
