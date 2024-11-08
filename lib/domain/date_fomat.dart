cc39e95d9de8305bd8a36a6e353220815f170e44
Suvorov <sertx.fallen@bk.ru>
2024-11-09 01:27:33 +0400

    9 11

--- lib/domain/date_fomat.dart
+++ lib/domain/date_fomat.dart
@@ -4,11 +4,7 @@ import 'package:week_number/iso.dart';
 String yearWeekNow() {
   final now = DateTime.now();
   String year = DateFormat('yyyy').format(now);
-  print(year);
   int week = now.weekNumber;
-
-
-
 
   return '$year$week';
 }
