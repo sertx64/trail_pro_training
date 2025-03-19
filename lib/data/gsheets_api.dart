d695e03b875e1387e7061d9946c16ef02baf50be
sertx <ssuvorov@nopreset.ru>
2025-03-19 16:26:44 +0400

    test

--- lib/data/gsheets_api.dart
+++ lib/data/gsheets_api.dart
@@ -4,7 +4,7 @@ import 'package:trailpro_planning/domain/management.dart';
 class ApiGSheet {
   final Spreadsheet ss = Management.forGSheetsApi;
 
-//test
+//test123
 
   Future<List<String>?> getUserData(String login) async {
     final sheet = ss.worksheetByTitle('users');
