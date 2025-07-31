a63b8a91b23447655b8c2f51953ed0c8e07884d8
sertx <ssuvorov@nopreset.ru>
2025-07-31 16:34:03 +0400

    переход на флаттер 3.32.0

--- lib/presentation/theme/app_theme.dart
+++ lib/presentation/theme/app_theme.dart
@@ -29,7 +29,7 @@ class AppTheme {
     ),
 
     // Card Theme
-    cardTheme: CardTheme(
+    cardTheme: CardThemeData(
       elevation: 4,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(16),
@@ -149,7 +149,7 @@ class AppTheme {
     ),
 
     // Dialog Theme
-    dialogTheme: DialogTheme(
+    dialogTheme: DialogThemeData(
       backgroundColor: AppColors.background,
       elevation: 16,
       shape: RoundedRectangleBorder(
